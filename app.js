b9b439a6bb240fac91ebdfa1c9ed3551a7d77e57
yustarandomname <abeldebruijn@hotmail.com>
2020-01-14 17:04:00 +0100

    added routes folder and routing capability

--- app.js
+++ app.js
@@ -4,5 +4,21 @@ var http = require('http');
 var port = process.argv[2];
 var app = express();
 
+// Set static files
 app.use(express.static(__dirname + '/public'));
-http.createServer(app).listen(port);
+
+// Reroute / to splash.html
+app.get('/', function(req, res) {
+	res.sendFile('splash.html', { root: './public/routes' });
+});
+
+// Reroute /game to game.html
+app.get('/game', function(req, res) {
+	// console.log(__dirname + '/public/routes/splash.html');
+	res.sendFile('game.html', { root: './public/routes' });
+});
+
+//start server
+http.createServer(app).listen(port, () => {
+	console.log(`The server has started at port: ${port}`);
+});
